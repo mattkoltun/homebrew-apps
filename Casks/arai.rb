a34161069ccdfd57e9c911f87ab25beef77104f7
mkoltun_vectra <mkoltun@vectra.ai>
2026-03-26 09:05:04 +0000

    main updated sha

--- Casks/arai.rb
+++ Casks/arai.rb
@@ -1,6 +1,6 @@
 cask "arai" do
   version "0.18.0"
-  sha256 "sha256:b616ad80bbdc3abb4997689a717a1210cada4eb2ca716dcde2306587de7c8a3b"
+  sha256 "b616ad80bbdc3abb4997689a717a1210cada4eb2ca716dcde2306587de7c8a3b"
 
   url "https://github.com/mattkoltun/arai/releases/download/v#{version}/Arai-#{version}-macos-universal.dmg"
   name "Arai"
